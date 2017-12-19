7739edda269ddc89f5820292dce42869265e8e31
Marcus Sellmann <healingkahn@gmail.com>
2017-12-19 21:21:10 +0100

    Added unit test project for the data package.

---
+++ AirlineManagerDataTest/Tests/PlayerTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AirlineManager.Data;
+
+namespace AirlineManagerDataTest {
+	[TestClass]
+	public class PlayerTest {
+		[TestMethod]
+		public void TestCorrectPlayerCreation() {
+			Player p = new Player("Test Player");
+
+			Assert.IsNotNull(p);
+			Assert.IsTrue(p.Name == "Test Player");
+			Assert.IsTrue(p.CarriedCargo == 0);
+			Assert.IsTrue(p.CarriedPax == 0);
+			Assert.IsTrue(p.OperatedFlights == 0);
+			Assert.IsTrue(p.TotalDistanceFlown == 0);
+		}
+
+		[TestMethod]
+		public void TestPlayerCreationWithEmptyName() {
+			Player p = new Player("");
+
+			Assert.Fail();
+		}
+	}
+}
