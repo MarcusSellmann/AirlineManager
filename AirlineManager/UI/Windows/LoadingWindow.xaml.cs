622403810eca3369d84141a7f9ca523e2503fdb1
Marcus Sellmann <healingkahn@gmail.com>
2017-12-21 21:46:18 +0100

    Inserted a loading window.

---
+++ AirlineManager/UI/Windows/LoadingWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace AirlineManager.UI.Windows {
+	/// <summary>
+	/// Interaction logic for LoadingWindow.xaml
+	/// </summary>
+	public partial class LoadingWindow : Window {
+		private int m_progress;
+
+		public LoadingWindow() {
+			InitializeComponent();
+		}
+	}
+}
