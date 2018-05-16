2eefa3ad540fa7dc88a3c44f5da684134bbdf020
Marcus Sellmann <marcus.sellmann@iav.de>
2018-05-16 16:25:43 +0200

    Updated the airport database to contain all german international airport plus the already existing three american airports.

--- AirlineManagerBusinessLogic/Business/Databases/AirportDatabase.cs
+++ AirlineManagerBusinessLogic/Business/Databases/AirportDatabase.cs
@@ -32,30 +32,26 @@ namespace AirlineManager.Business.Databases {
 		AirportDatabase() {
             DB = new List<Airport>();
 
-			// TODO: Extend database by reading from file.
-			ICAOCode iEDDT = new ICAOCode("EDDT");
-			ICAOCode iEDDF = new ICAOCode("EDDF");
-			ICAOCode iKJFK = new ICAOCode("KJFK");
-			ICAOCode iKLAX = new ICAOCode("KLAX");
-			ICAOCode iKATL = new ICAOCode("KATL");
-
-			GeoCoordinate gcEDDT = new GeoCoordinate(52.559686, 13.287711);
-			GeoCoordinate gcEDDF = new GeoCoordinate(50.033333, 8.570556);
-			GeoCoordinate gcKJFK = new GeoCoordinate(40.63975, -73.778925);
-			GeoCoordinate gcKLAX = new GeoCoordinate(33.942536, -118.408075);
-			GeoCoordinate gcKATL = new GeoCoordinate(33.639167, -84.427778);
-
-			Airport aEDDT = new Airport(iEDDT, "TXL", "Berlin-Tegel", "Berlin", 3023, gcEDDT);
-			Airport aEDDF = new Airport(iEDDF, "FRA", "Frankfurt am Main", "Frankfurt am Main", 4000, gcEDDF);
-			Airport aKJFK = new Airport(iKJFK, "JFK", "John F. Kennedy International Airport", "New York", 4423, gcKJFK);
-			Airport aKLAX = new Airport(iKLAX, "LAX", "Los Angeles International Airport", "Los Angeles", 3685, gcKLAX);
-			Airport aKATL = new Airport(iKATL, "ATL", "Hartsfield–Jackson Atlanta International Airport", "Atlanta", 3624, gcKATL);
-
-            DB.Add(aEDDT);
-			DB.Add(aEDDF);
-			DB.Add(aKJFK);
-			DB.Add(aKLAX);
-			DB.Add(aKATL);
-		}
-	}
+            // TODO: Extend database by reading from file.
+            DB.Add(new Airport(new ICAOCode("EDDB"), "SXF", "Flughafen Berlin-Schönefeld", "Berlin", 3600, new GeoCoordinate(52.388333, 13.52)));
+            DB.Add(new Airport(new ICAOCode("EDDC"), "DRS", "Flughafen Dresden", "Dresden", 2850, new GeoCoordinate(51.134344, 13.768)));
+            DB.Add(new Airport(new ICAOCode("EDDE"), "ERF", "Flughafen Erfurt-Weimar", "Erfurt", 2600, new GeoCoordinate(50.979811, 10.958106)));
+            DB.Add(new Airport(new ICAOCode("EDDF"), "FRA", "Flughafen Frankfurt am Main", "Frankfurt am Main", 4000, new GeoCoordinate(50.033333, 8.570556)));
+            DB.Add(new Airport(new ICAOCode("EDDG"), "FMO", "Münster Osnabrück International Airport", "Münster", 2170, new GeoCoordinate(52.134642, 7.684831)));
+            DB.Add(new Airport(new ICAOCode("EDDH"), "HAM", "Flughafen Hamburg", "Hamburg", 3666, new GeoCoordinate(53.630278, 9.988333)));
+            DB.Add(new Airport(new ICAOCode("EDDK"), "CGN", "Flughafen Köln/Bonn 'Konrad Adenauer'", "Köln", 3815, new GeoCoordinate(50.865917, 7.142744)));
+            DB.Add(new Airport(new ICAOCode("EDDL"), "DUS", "Düsseldorf Airport", "Düsseldorf", 3000, new GeoCoordinate(51.280925, 6.757311)));
+            DB.Add(new Airport(new ICAOCode("EDDM"), "MUC", "Flughafen München 'Franz Josef Strauß'", "München", 4000, new GeoCoordinate(48.353783, 11.786086)));
+            DB.Add(new Airport(new ICAOCode("EDDN"), "NUE", "Flughafen Nürnberg 'Albrecht Dürer'", "Nürnberg", 2700, new GeoCoordinate(49.4987, 11.078008)));
+            DB.Add(new Airport(new ICAOCode("EDDP"), "LEJ", "Leipzig/Halle Airport", "Leipzig", 3600, new GeoCoordinate(51.423992, 12.236356)));
+            DB.Add(new Airport(new ICAOCode("EDDR"), "SCN", "Flughafen Saarbrücken", "Saarbrücken", 2000, new GeoCoordinate(49.214553, 7.109508)));
+            DB.Add(new Airport(new ICAOCode("EDDS"), "STR", "Flughafen Stuttgart - Manfred Rommel Flughafen", "Stuttgart", 3345, new GeoCoordinate(48.689878, 9.221964)));
+            DB.Add(new Airport(new ICAOCode("EDDT"), "TXL", "Flughafen Berlin-Tegel 'Otto Lilienthal'", "Berlin", 3023, new GeoCoordinate(52.559686, 13.287711)));
+            DB.Add(new Airport(new ICAOCode("EDDV"), "HAJ", "Flughafen Hannover-Langenhagen", "Hannover", 3200, new GeoCoordinate(52.461056, 9.685078)));
+            DB.Add(new Airport(new ICAOCode("EDDW"), "BRE", "Flughafen Bremen", "Bremen", 2040, new GeoCoordinate(53.0475, 8.786667)));
+            DB.Add(new Airport(new ICAOCode("KATL"), "ATL", "Hartsfield–Jackson Atlanta International Airport", "Atlanta", 3624, new GeoCoordinate(33.639167, -84.427778)));
+            DB.Add(new Airport(new ICAOCode("KJFK"), "JFK", "John F. Kennedy International Airport", "New York City", 4423, new GeoCoordinate(40.63975, -73.778925)));
+            DB.Add(new Airport(new ICAOCode("KLAX"), "LAX", "Los Angeles International Airport", "Los Angeles", 3685, new GeoCoordinate(33.942536, -118.408075)));
+        }
+    }
 }
