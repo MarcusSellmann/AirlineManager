b6de3a81876a3b6925fab055ecd413dcdb94be35
Marcus Sellmann <marcus.sellmann@iav.de>
2018-05-17 12:20:55 +0200

    #7: Added first version of the Gantt chart which works as the graphical overview of the maintenance services planned.

--- AirlineManager/UI/Pages/MaintenancePage.xaml.cs
+++ AirlineManager/UI/Pages/MaintenancePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,116 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using nGantt;
+using nGantt.GanttChart;
+using nGantt.PeriodSplitter;
+
+using AirlineManager.Data;
+using AirlineManager.UI.Utilities;
+using AirlineManager.Business;
 
 namespace AirlineManager.UI.Pages {
     /// <summary>
     /// Interaktionslogik für MaintenancePage.xaml
     /// </summary>
     public partial class MaintenancePage : Page {
+        readonly CultureInfo culture = new CultureInfo("de-DE");
+        public List<MaintenanceServiceContainer> chartData { get; private set; } = new List<MaintenanceServiceContainer>();
+
         public MaintenancePage() {
+            chartData = new List<MaintenanceServiceContainer>();
+
             InitializeComponent();
 
+            Dictionary<AircraftInstance, List<PlannedService>> serviceData = MainGameController.Instance.CurrentAirline.AllPlannedServices;
+
+            foreach(KeyValuePair<AircraftInstance, List<PlannedService>> sd in serviceData) {
+                foreach(PlannedService ps in sd.Value) {
+                    chartData.Add(new MaintenanceServiceContainer(sd.Key.ToString(), ps));
+                }
+            }
+
+            chartData.Add(new MaintenanceServiceContainer("D-NAGZ", new PlannedService(ServiceLevel.A, DateTime.Now.AddMinutes(20), TimeSpan.FromHours(2), 545632)));
+            chartData.Add(new MaintenanceServiceContainer("D-BGAH", new PlannedService(ServiceLevel.C, DateTime.Now.AddHours(1), TimeSpan.FromHours(13), 2548545632)));
+            chartData.Add(new MaintenanceServiceContainer("D-AHFD", new PlannedService(ServiceLevel.B, DateTime.Now.AddHours(1).AddMinutes(10), TimeSpan.FromHours(7), 25485632)));
+
+            DateTime minDate = DateTime.Now;
+
+            if (chartData.Count > 0) {
+                minDate = chartData.Min(s => s.Service.StartTime).AddHours(-minDate.Hour);
+            }
             
+            ConvertData(minDate, minDate.AddDays(1));
+        }
+
+        public void ConvertData(DateTime minDate, DateTime maxDate) {
+            // Set max and min dates
+            ganttUserControl.Initialize(minDate, maxDate);
+
+            // Create timelines and define how they should be presented
+            ganttUserControl.CreateTimeLine(new PeriodYearSplitter(minDate, maxDate), p => p.Start.Year.ToString());
+            ganttUserControl.CreateTimeLine(new PeriodMonthSplitter(minDate, maxDate), p => p.Start.Month.ToString());
+            ganttUserControl.CreateTimeLine(new PeriodDaySplitter(minDate, maxDate), p => p.Start.Day.ToString());
+            var gridLineTimeLine = ganttUserControl.CreateTimeLine(new PeriodHourSplitter(minDate, maxDate), p => p.Start.Hour.ToString());
+
+            // Set the timeline to attach gridlines to
+            ganttUserControl.SetGridLinesTimeline(gridLineTimeLine, DetermineBackground);
+
+            HeaderedGanttRowGroup servicesLevelA = ganttUserControl.CreateGanttRowGroup("Level A");
+            HeaderedGanttRowGroup servicesLevelB = ganttUserControl.CreateGanttRowGroup("Level B");
+            HeaderedGanttRowGroup servicesLevelC = ganttUserControl.CreateGanttRowGroup("Level C");
+            HeaderedGanttRowGroup servicesLevelD = ganttUserControl.CreateGanttRowGroup("Level D");
+            HeaderedGanttRowGroup servicesExtras = ganttUserControl.CreateGanttRowGroup("Extras");
+
+            foreach (MaintenanceServiceContainer plannedService in chartData) {
+                GanttRow row = null;
+
+                switch (plannedService.Service.Level) {
+                    case ServiceLevel.A:
+                        row = ganttUserControl.CreateGanttRow(servicesLevelA, plannedService.Name);
+                        break;
+
+                    case ServiceLevel.B:
+                        row = ganttUserControl.CreateGanttRow(servicesLevelB, plannedService.Name);
+                        break;
+
+                    case ServiceLevel.C:
+                        row = ganttUserControl.CreateGanttRow(servicesLevelC, plannedService.Name);
+                        break;
+
+                    case ServiceLevel.D:
+                        row = ganttUserControl.CreateGanttRow(servicesLevelD, plannedService.Name);
+                        break;
+
+                    default:
+                        row = ganttUserControl.CreateGanttRow(servicesExtras, plannedService.Name);
+                        break;
+                }
+
+                List<MaintenanceServiceContainer> sortedChartTimeSpan = chartData.FindAll(p => p.Name.Equals(plannedService.Name));
+
+                foreach (MaintenanceServiceContainer sorted in sortedChartTimeSpan) {
+                    ganttUserControl.AddGanttTask(row, new GanttTask {
+                        Start = sorted.Service.StartTime,
+                        End = sorted.Service.EndTime,
+                        Name = sorted.Name,
+                        Radius = sorted.Service.Duration.TotalDays < 3 ? 0 : 5,
+                        Color = sorted.Selected ? Colors.White : Colors.DodgerBlue,
+                        TaskProgressVisibility = Visibility.Hidden
+                    });
+                }
+            }
+        }
+
+        private Brush DetermineBackground(TimeLineItem timeLineItem) {
+            if (timeLineItem.End.Date.DayOfWeek == DayOfWeek.Saturday || timeLineItem.End.Date.DayOfWeek == DayOfWeek.Sunday) {
+                return new SolidColorBrush(Colors.White);
+            }
+            
+            return new SolidColorBrush(Colors.Transparent);
+        }
+
+        private void btnAddMaintenanceService_Click(object sender, RoutedEventArgs e) {
+
         }
     }
 }
---
+++ AirlineManager/UI/Utilities/MaintenanceServiceContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AirlineManager.Data;
+
+namespace AirlineManager.UI.Utilities {
+    public class MaintenanceServiceContainer {
+        #region Properties
+        public string Name { get; private set; }
+        public bool Selected { get; private set; }
+        public PlannedService Service { get; private set; }
+        #endregion
+
+        public MaintenanceServiceContainer(string name, PlannedService service, bool selected = false) {
+            Name = name;
+            Selected = selected;
+            Service = service;
+        }
+    }
+}
