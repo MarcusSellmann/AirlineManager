39c431276f8d94d48be8b4056454e30c572c03d3
Marcus Sellmann <marcus.sellmann@iav.de>
2018-05-25 11:16:05 +0200

    Resolves #15: Added a popover window which asks the user for an override confirmation.

--- AirlineManager/UI/Pages/GameMenuPage.xaml.cs
+++ AirlineManager/UI/Pages/GameMenuPage.xaml.cs
@@ -1,19 +1,26 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+
 using AirlineManager.Business;
+using AirlineManager.UI.Interfaces;
+using AirlineManager.UI.Windows.Popover;
 
 namespace AirlineManager.UI.Pages
 {
     /// <summary>
     /// Interaction logic for GameMenuPage.xaml
     /// </summary>
-    public partial class GameMenuPage : Page {
+    public partial class GameMenuPage : Page, ISavegameOverrideInteractionListener {
         public GameMenuPage() {
             InitializeComponent();
         }
 
         private void btnSaveGame_Click(object sender, RoutedEventArgs e) {
-            MainGameController.Instance.SaveGame();
+            if (MainGameController.Instance.DoesSavegameExist()) {
+                new SavegameAlreadyExistsWindow(this).Show();
+            } else {
+                MainGameController.Instance.SaveGame();
+            }
         }
 
         private void btnLoadGame_Click(object sender, RoutedEventArgs e) {
@@ -23,6 +30,13 @@ namespace AirlineManager.UI.Pages
 
         private void btnExitGame_Click(object sender, RoutedEventArgs e) {
             
+        }
+
+        public void overrideConfirmed() {
+            MainGameController.Instance.SaveGame();
+        }
+
+        public void overrideAborted() {
         }
     }
 }
---
+++ AirlineManager/UI/Windows/Popover/SavegameAlreadyExistsWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AirlineManager.UI.Interfaces;
+
+using MahApps.Metro.Controls;
+
+namespace AirlineManager.UI.Windows.Popover {
+    /// <summary>
+    /// Interaktionslogik für SavegameAlreadyExistsWindow.xaml
+    /// </summary>
+    public partial class SavegameAlreadyExistsWindow : MetroWindow {
+        #region Attributes
+        private ISavegameOverrideInteractionListener m_listener = null;
+        #endregion
+
+        public SavegameAlreadyExistsWindow(ISavegameOverrideInteractionListener listener) {
+            m_listener = listener;
+
+            InitializeComponent();
+        }
+
+        private void btnAbort_Click(object sender, System.Windows.RoutedEventArgs e) {
+            m_listener.overrideAborted();
+            Close();
+        }
+
+        private void btnConfirm_Click(object sender, System.Windows.RoutedEventArgs e) {
+            m_listener.overrideConfirmed();
+            Close();
+        }
+    }
+}
